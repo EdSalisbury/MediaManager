06a9c9e91a8aa550e977021761748a5a4408e2f4
Ed Salisbury <ed.salisbury@gmail.com>
2024-12-11 22:02:17 -0700

    Added support for new localdb module

--- mediamanager/core.py
+++ mediamanager/core.py
@@ -11,18 +11,19 @@ from localdb import LocalDB
 
 logger = logging.getLogger("mediamanager")
 
+
 class MediaManager:
     def __init__(self):
         self.cfg = None
         self.db = None
 
     def init_db(self):
-        self.db = LocalDB(self.cfg.get('database_file'))
+        self.db = LocalDB(self.cfg.get("database_file"), "sqlite3")
         self.db.__enter__()
 
     def get_cfg_path(self):
         home_dir = os.path.expanduser("~")
-        return os.path.join(home_dir, f".mediamanager.cfg.json")    
+        return os.path.join(home_dir, f".mediamanager.cfg.json")
 
     def load_cfg(self):
         cfg_file = self.get_cfg_path()
@@ -31,10 +32,10 @@ class MediaManager:
                 self.cfg = json.load(cfg_file)
         except FileNotFoundError:
             raise FileNotFoundError(f"Configuration file not found at {cfg_file}")
-            
+
     def save_cfg(self, config):
         cfg_file = self.get_cfg_path()
-        with open(cfg_file, 'w') as file:
+        with open(cfg_file, "w") as file:
             json.dump(config, file, indent=4)
         self.cfg = self.load_cfg()
 
@@ -52,10 +53,8 @@ class MediaManager:
 
         filename = os.path.basename(path)
         new_path = os.path.join(
-            self.cfg.get("media_directory"),
-            md.get('year'),
-            new_folder,
-            filename)
+            self.cfg.get("media_directory"), md.get("year"), new_folder, filename
+        )
         new_path = utils.generate_unique_filename(new_path)
 
         folder = os.path.dirname(new_path)
@@ -88,14 +87,14 @@ class MediaManager:
                         return True
                     else:
                         logger.info(
-                            f"Duplicate detected for {path} (original is {db_path})")
+                            f"Duplicate detected for {path} (original is {db_path})"
+                        )
                         if move_duplicates:
                             return self.move_to_duplicate(path)
                         else:
                             return True
                 else:
-                    logger.debug(
-                        f"Replacing record for {db_path} ({hash})")
+                    logger.debug(f"Replacing record for {db_path} ({hash})")
                     return self.db.save_value(hash, path)
             else:
                 logger.debug(f"Adding record for {path} ({hash})")
@@ -127,8 +126,7 @@ class MediaManager:
             hash = utils.get_hash(path)
             db_path = self.db.load_value(hash)
             if db_path and os.path.isfile(db_path):
-                logger.info(
-                    f"File has already been imported {path} ({hash}) {db_path}")
+                logger.info(f"File has already been imported {path} ({hash}) {db_path}")
                 return True
             else:
                 logger.info(f"Importing file {path} ({hash})")
@@ -140,8 +138,9 @@ class MediaManager:
             logger.error(traceback.format_exc())
             return False
 
-    def process_dir(self, folder, max_workers=4,
-                    import_files=False, move_duplicates=False):
+    def process_dir(
+        self, folder, max_workers=4, import_files=False, move_duplicates=False
+    ):
         logger.info(f"Analyzing directory {folder}.")
         paths = os.listdir(folder)
         files = list()
@@ -158,7 +157,8 @@ class MediaManager:
                 folder,
                 max_workers=max_workers,
                 import_files=import_files,
-                move_duplicates=move_duplicates)
+                move_duplicates=move_duplicates,
+            )
 
         mtime = os.path.getmtime(folder)
         last_modified = self.db.load_value(folder)
@@ -169,20 +169,20 @@ class MediaManager:
                     partial_process_file = partial(self.import_file)
                 else:
                     partial_process_file = partial(
-                        self.process_file,
-                        move_duplicates=move_duplicates)
+                        self.process_file, move_duplicates=move_duplicates
+                    )
 
                 futures = [
-                    executor.submit(
-                        partial_process_file,
-                        file) for file in files]
+                    executor.submit(partial_process_file, file) for file in files
+                ]
 
                 for future in as_completed(futures):
                     try:
                         result = future.result()  # Wait for each future to complete
                         if not result:
                             logger.error(
-                                f"Processing failed for file: {future.exception()}")
+                                f"Processing failed for file: {future.exception()}"
+                            )
                     except Exception as e:
                         logger.error(f"Error processing file: {e}")
                         logger.error(traceback.format_exc())
--- setup.py
+++ setup.py
@@ -1,22 +1,51 @@
 from setuptools import setup, find_packages
+from os import path
+
+loc = path.abspath(path.dirname(__file__))
+
+with open(loc + "/requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+required = []
+dependency_links = []
+
+# Do not add to required lines pointing to Git repositories
+EGG_MARK = "#egg="
+for line in requirements:
+    if (
+        line.startswith("-e git:")
+        or line.startswith("-e git+")
+        or line.startswith("git:")
+        or line.startswith("git+")
+    ):
+        line = line.lstrip("-e ")  # in case that is using "-e"
+        if EGG_MARK in line:
+            package_name = line[line.find(EGG_MARK) + len(EGG_MARK) :]
+            repository = line[: line.find(EGG_MARK)]
+            required.append("%s @ %s" % (package_name, repository))
+            dependency_links.append(line)
+        else:
+            print("Dependency to a git repository should have the format:")
+            print("git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name")
+    else:
+        required.append(line)
 
 setup(
-    name='MediaManager',
-    version='1.0.0',
+    name="MediaManager",
+    version="1.1.0",
     packages=find_packages(),
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    scripts=[
-        'bin/mediamanager'
-    ],
-    author='Ed Salisbury',
-    author_email='ed.salisbury@gmail.com',
-    description='A tool for managing photo and video files.',
-    license='MIT',
-    url='https://github.com/edsalisbury/MediaManager',
+    install_requires=required,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    scripts=["bin/mediamanager"],
+    author="Ed Salisbury",
+    author_email="ed.salisbury@gmail.com",
+    description="A tool for managing photo and video files.",
+    license="MIT",
+    url="https://github.com/edsalisbury/MediaManager",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
